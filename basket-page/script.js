e85f34674b5183419188debf224422e6464ee33c
Юрий <avalanchedoto@mail.ru>
2025-05-25 00:37:33 +0300

    25.05.25 item page commit

--- basket-page/script.js
+++ basket-page/script.js
@@ -8,32 +8,87 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const minusButton = document.querySelector('.button-minus')
     const plusButton = document.querySelector('.button-plus')
+    const BasketQuantity = document.querySelector('.productQuantity')
+
+    const productQuantity = document.querySelector('.basket-price__current');
+    const priceText = productQuantity.textContent.replace(/\s/g, '').replace('р', '');
+    const basePrice = parseInt(priceText, 10);
+
+    const oldPrice = document.querySelector('.basket-price__extras-old');
+    const oldPriceText = oldPrice.textContent.replace(/\s/g, '').replace('р', '');
+    const oldPriceParse = parseInt(oldPriceText, 10);
+
+    const finalPriceItem = document.querySelector('.finalPrice');
+    const finalPriceItemText = finalPriceItem.textContent.replace(/\s/g, '').replace('р', '');
+    const finalPriceItemParse = parseInt(finalPriceItemText, 10);
+
+    const finalPrice = document.querySelector('.justify-line__desc');
+    const finalPriceText = finalPrice.textContent.replace(/\s/g, '').replace('р', '');
+    const finalPriceParse = parseInt(finalPriceText, 10);
+
+    const redPrice = document.querySelector('.red');
+    const redPriceText = redPrice.textContent.replace(/\s/g, '').replace('р', '').replace('-', '');
+    const redPriceParse = parseInt(redPriceText, 10);
+
+
+
+
+
+
+
+
+
 
 
 
     minusButton.addEventListener('click', function () {
         input.stepDown();
         updateMinusColor();
+        updatePrice()
+
     });
 
     plusButton.addEventListener('click', function () {
         input.stepUp();
         updateMinusColor();
+        updatePrice()
+
     });
 
     function updateMinusColor() {
         if (Number(input.value) > 1) {
             minusSvg.style.color = '#303030'
-            console.log("Черный")
         }
         else {
             minusSvg.style.color = '#c7c7c7'
-            console.log("Серый")
         }
+    }
+    function updatePrice() {
+        const quantity = Number(input.value);
+
+        const currentTotalPrice = basePrice * quantity;
+        productQuantity.textContent = `${currentTotalPrice.toLocaleString('ru-RU')}р`;
+
+        const oldTotalPrice = oldPriceParse * quantity;
+        oldPrice.textContent = `${oldTotalPrice.toLocaleString('ru-RU')}р`;
+
+        const finalTotalPriceItem = finalPriceItemParse * quantity;
+        finalPriceItem.textContent = `${finalTotalPriceItem.toLocaleString('ru-RU')}р`;
+
+        const finalTotalPrice = finalPriceParse * quantity;
+        finalPrice.textContent = `${finalTotalPrice.toLocaleString('ru-RU')}р`;
+
+        const redTotalPrice = redPriceParse * quantity;
+        redPrice.textContent = `-${redTotalPrice.toLocaleString('ru-RU')}р`;
+
+        BasketQuantity.textContent = String(quantity);
+
     }
 
 
+
     updateMinusColor();
+    updatePrice();
 
 
 
